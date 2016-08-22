7f598625697999d34191a7b152106678542afad4
Amol Bharmoria <AmBhar@Amols-MacBook-Air.local>
2016-08-23 00:28:46 +0530

    Remove extra files

--- Recursion/SortArray.py
+++ Recursion/SortArray.py
@@ -9,3 +9,9 @@ def isArraySorted(array):
 
 myArray =[4,7,8,10,16,12,9,13,10,15]
 print(isArraySorted(myArray))
+
+# Time Complexity is O(n) 
+# Space Complexity is O(n) for recursive stack space 
+
+# array[1:] means starting from index 1 to end
+# array[1:5] means starting from index 1 to index 5 of array
